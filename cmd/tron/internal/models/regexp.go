5a7240472d6c4555870d8c008025efe70fd24a53
lissteron <lissteron@gmail.com>
2020-10-13 19:54:13 +0300

    fix regexp

--- cmd/tron/internal/models/regexp.go
+++ cmd/tron/internal/models/regexp.go
@@ -6,14 +6,12 @@ import (
 
 // nolint:gochecknoglobals //regexp
 var (
-	VersionRegexp = regexp.MustCompile(
-		`([0-9]+)\.([0-9]+)(\.([0-9]+))?(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?`,
-	)
-	ImportRegexp  = regexp.MustCompile(`^import "(.*?)";$`)
-	ServiceRegexp = regexp.MustCompile(`^service (.*?) {`)
-	PackageRegexp = regexp.MustCompile(`^package[\s]*?(\w*);$`)
-	ModuleRegexp  = regexp.MustCompile(`^module (.*)$`)
+	VersionRegexp = regexp.MustCompile(`(\d+\.\d+\.\d+)`)
+	ImportRegexp  = regexp.MustCompile(`^import "([^"]+)";$`)
+	ServiceRegexp = regexp.MustCompile(`^service (\S+) {`)
+	PackageRegexp = regexp.MustCompile(`^package ([^;]+);$`)
+	ModuleRegexp  = regexp.MustCompile(`^module (.+)$`)
 
-	GoNameRexp      = regexp.MustCompile(`[^a-zA-Z0-9\\s_-]+`)
-	FirstDigitsRexp = regexp.MustCompile(`^\\d+`)
+	GoNameRexp      = regexp.MustCompile(`[^a-zA-Z0-9\s_-]+`)
+	FirstDigitsRexp = regexp.MustCompile(`^\d+`)
 )
