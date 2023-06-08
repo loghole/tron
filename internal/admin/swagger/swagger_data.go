1b8c5402daee0dca42a77d6aafba33a7ae5b12f4
Gleb Dubrovin <termiusone@gmail.com>
2023-06-08 17:48:20 +0300

    update swagger

--- internal/admin/swagger/swagger_data.go
+++ internal/admin/swagger/swagger_data.go
@@ -6,12 +6,15 @@ import "embed"
 //
 //go:embed favicon-16x16.png
 //go:embed favicon-32x32.png
+//go:embed index.css
 //go:embed index.html
 //go:embed oauth2-redirect.html
+//go:embed swagger-initializer.js
 //go:embed swagger-ui-bundle.js
 //go:embed swagger-ui-es-bundle-core.js
 //go:embed swagger-ui-es-bundle.js
 //go:embed swagger-ui-standalone-preset.js
 //go:embed swagger-ui.css
 //go:embed swagger-ui.js
+//go:embed swagger_data.go
 var Content embed.FS
